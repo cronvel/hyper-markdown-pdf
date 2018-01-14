ccabd93ba97c814f1cc1cb05473ac315a6941c70
Cédric Ronvel <cedric.ronvel@gmail.com>
2018-01-14 20:41:24 +0100

    wip

--- sample/test.js
+++ sample/test.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
 var pdfMake = require( 'pdfmake' ) ;
-var htmlToPdf = require( '..' ) ;
+var HtmlToPdf = require( '..' ) ;
 
 
 var raw = "normal <i>italic</i> normal" ;
-var htmlDoc = htmlToPdf.parse( raw ) ;
+var htmlDoc = HtmlToPdf.parse( raw ) ;
 
 console.log( htmlDoc.parts ) ;
 console.log() ;
@@ -18,7 +18,36 @@ console.log() ;
 
 
 var pdfDoc = {
-	content: null
+	styles: HtmlToPdf.defaultStyles ,
+	content: output
 } ;
+
+console.log( pdfDoc ) ;
+console.log() ;
+
+
+
+// pdfmake things
+
+var fonts = {
+	Roboto: {
+		normal: './fonts/Roboto-Regular.ttf',
+		bold: './fonts/Roboto-Medium.ttf',
+		italics: './fonts/Roboto-Italic.ttf',
+		bolditalics: './fonts/Roboto-Italic.ttf'
+	}
+};
+
+var PdfPrinter = require('pdfmake/src/printer');
+var printer = new PdfPrinter(fonts);
+
+var pdfDoc = printer.createPdfKitDocument( pdfDoc );
+pdfDoc.pipe(fs.createWriteStream('test.pdf')).on('finish',function(){
+    //success
+});
+pdfDoc.end();
+
+
+
 
 
