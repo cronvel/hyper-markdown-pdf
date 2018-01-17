479bef3798ee8931216d4c0a1068a00d6de53625
Cédric Ronvel <cedric.ronvel@gmail.com>
2018-01-17 12:23:27 +0100

    wip

--- sample/pdfkit.js
+++ sample/pdfkit.js
@@ -5,11 +5,12 @@ var HtmlToPdf = require( '..' ) ;
 
 
 var raw = "<h1>Title</h1><p>normal <i>italic</i> normal</p><i>italic2</i>" ;
+var raw = "Some text" ;
 var htmlDoc = HtmlToPdf.parse( raw ) ;
 
 console.log( htmlDoc.parts ) ;
 console.log() ;
-console.log( htmlDoc.parts[1].subParts ) ;
+//console.log( htmlDoc.parts[1].subParts ) ;
 console.log() ;
 
 /*
@@ -19,12 +20,5 @@ console.log() ;
 */
 
 
-var pdfStream = htmlDoc.createPdfStream() ;
-
-pdfStream.pipe( fs.createWriteStream( 'test.pdf' ) ).on( 'finish' , () => {
-    //success
-} ) ;
-
-pdfStream.end() ;
-
+htmlDoc.pdfKitRender() ;
 
