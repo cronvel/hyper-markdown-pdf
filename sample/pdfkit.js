4dac4707a2b06427aefd55b674a516bea127477a
Cédric Ronvel <cedric.ronvel@gmail.com>
2018-01-17 19:27:12 +0100

    wip

--- sample/pdfkit.js
+++ sample/pdfkit.js
@@ -4,8 +4,8 @@ var fs = require( 'fs' ) ;
 var HtmlToPdf = require( '..' ) ;
 
 
-var raw = "<h1>Title</h1><p>normal <i>italic</i> normal</p><i>italic2</i>" ;
-var raw = "Some text" ;
+var raw = "<h1>Title</h1><p>normal <u>underline</u> normal</p><u>underline 2</u>" ;
+var raw = "Some text, <u>some underline test</u>" ;
 var htmlDoc = HtmlToPdf.parse( raw ) ;
 
 console.log( htmlDoc.parts ) ;
