effdadb815475c5e4103cc6e5aa529625339c6e9
Cédric Ronvel <cedric.ronvel@gmail.com>
2018-01-26 19:41:43 +0100

    wip

--- sample/renderer2-test.js
+++ sample/renderer2-test.js
@@ -48,24 +48,33 @@ var pdfDoc = new metaPdf.PdfDocument() ;
 var block = new elements.FlowBlock( pdfDoc , {
 	width: 300 ,
 	height: 300 ,
-//	align: 'topLeft' ,
+	align: 'topLeft' ,
 //	align: 'topRight' ,
-	align: 'top' ,
+//	align: 'top' ,
+	justify: true
 } ) ;
 
 var text ;
-//text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" ;
-text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium" ;
+//text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? " ;
+//text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium" ;
 text = "Sed ut abcdefghijklmnopqrstuvwxyzperspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium" ;
 //text = "Sed ut perspiciatis unde omnis iste" ;
-//text = "bob bill jack joe john jacky" ;
+//text = "Bob Bill Jack Joe John Jacky. " ;
 //text = "bob" ;
 
-block.appendText( text , { font: "Helvetica" , fontSize: 36 } ) ;
+//text = text.repeat( 3 ) ;
+
+for ( let i = 0 ; i < 8 ; i ++ ) {
+	//block.appendText( text , { font: "Helvetica" , fontSize: 36 } ) ;
+	block.appendText( '#' + i + ': ' + text , { font: i % 2 ? "Helvetica" : "Helvetica-Oblique" , fontSize: 10 + i } ) ;
+}
+
 
 deb( block ) ;
 
 block.render() ;
+
+deb( block ) ;
 
 pdfDoc.pdfKitDoc.pipe( fs.createWriteStream( 'test.pdf' ) ) ;
 pdfDoc.pdfKitDoc.end() ;
