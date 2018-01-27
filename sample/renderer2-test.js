5e939e5895aef94ac31d331b7d3950494975d87e
Cédric Ronvel <cedric.ronvel@gmail.com>
2018-01-27 11:12:03 +0100

    wip

--- sample/renderer2-test.js
+++ sample/renderer2-test.js
@@ -45,7 +45,10 @@ function deb( obj ) {
 //var renderer = new metaPdf.PdfRenderer2() ;
 
 var pdfDoc = new metaPdf.PdfDocument() ;
-var block = new elements.FlowBlock( pdfDoc , {
+
+var master = new elements.MultiPageMaster( pdfDoc , { width: 500 , height: 500 } ) ;
+
+var block = new elements.FlowBlock( master , {
 	width: 300 ,
 	height: 300 ,
 	align: 'topLeft' ,
@@ -53,6 +56,11 @@ var block = new elements.FlowBlock( pdfDoc , {
 //	align: 'top' ,
 	justify: true
 } ) ;
+
+master.newPage() ;
+master.newPage() ;
+master.appendBox( block ) ;
+master.newPage() ;
 
 var text ;
 //text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? " ;
@@ -72,7 +80,7 @@ for ( let i = 0 ; i < 8 ; i ++ ) {
 
 deb( block ) ;
 
-block.render() ;
+master.render() ;
 
 //deb( block ) ;
 
